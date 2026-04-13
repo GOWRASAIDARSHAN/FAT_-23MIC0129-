f9efea982d6f682d55b1e351fdc42ce568f691c0
GOWRASAIDARSHAN <gowrasai.darshan2023@vitstudent.ac.in>
2026-04-13 16:20:01 +0530

    Add files via upload

---
+++ studentdetails.java
@@ -0,0 +1,20 @@
+package studentdetails;
+
+public class studentdetails {
+	public static void main(String args[]) {
+		float a=40;
+		float b=30;
+		float c=a-b;
+		float d=(c/a)*100;
+		float e=100-d;
+		System.out.println("Student Name: GOWRA SAI DARSHAN \n");
+		System.out.println("Registration Number: 23MIC0129 \n");
+		System.out.println("Course Name: Application Development and Deployment Architecture \n");
+		System.out.println("Department: MTech Computer Science and Engineering \n");
+		System.out.println("Number of classes conducted: "+a);
+		System.out.println("Number of classes attended: "+b);
+		System.out.println("Number of classes missed: "+c);
+		System.out.println("Attendance Percentage: "+e);
+		System.out.println("Attendance Build Successful");
+	}
+}
